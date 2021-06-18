50c60d8a866538e0385621d8d3b52923a9147656
balaji-githubstore <balaji0017@gmail.com>
2021-06-18 21:09:21 +0530

    third

--- src/test/java/com/google/openemrbase/WebDriverWrapper.java
+++ src/test/java/com/google/openemrbase/WebDriverWrapper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+//browser config
 public class WebDriverWrapper {
 	protected WebDriver driver;
 
