feb8eb4cddd7466cdd5cbf453ec827898cd92b5e
balaji-githubstore <balaji0017@gmail.com>
2021-06-23 20:16:54 +0530

    june23 - 2

--- src/test/java/com/google/utilities/DataProviderUtils.java
+++ src/test/java/com/google/utilities/DataProviderUtils.java
@@ -1,7 +1,25 @@
 package com.google.utilities;
 
+import org.testng.annotations.DataProvider;
+
 public class DataProviderUtils {
 	
-	
+		@DataProvider
+		public Object[][] validData()
+		{
+			Object[][] main=new Object[3][2];
+			//i-->no of test case
+			//j-->no of parameter
+			main[0][0]="peter";
+			main[0][1]="peter123";
+			
+			main[1][0]="john";
+			main[1][1]="john123";
+			
+			main[2][0]="king";
+			main[2][1]="king123";
+			
+			return main;
+		}
 
 }
