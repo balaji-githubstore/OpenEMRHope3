4872dd13d6f7683ecea7f1642eef450f2443bf74
balaji-githubstore <balaji0017@gmail.com>
2021-06-23 20:33:33 +0530

    jun23-final

--- src/test/java/com/google/openemrtest/LoginTest.java
+++ src/test/java/com/google/openemrtest/LoginTest.java
@@ -5,18 +5,23 @@ import org.testng.annotations.Test;
 
 import com.google.openemrbase.WebDriverWrapper;
 import com.google.openemrpages.Loginpage;
+import com.google.utilities.DataProviderUtils;
 
 public class LoginTest extends WebDriverWrapper{ 
 	
-	@Test(priority=1)
-	public void validCredentialTest()
+	//admin,pass,English (Standard),Patient: None
+	//physician,physician,English (Standard),Patient: None
+	
+	
+	@Test(priority=1,dataProvider = "validCredentialData",dataProviderClass = DataProviderUtils.class)
+	public void validCredentialTest(String username,String password,String language,String expectedValue)
 	{
 		Loginpage login = new Loginpage(driver);
-		login.enterUserName("admin");
-		login.enterPassword("pass");
-		login.selectLanguage("English (Standard)");
+		login.enterUserName(username);
+		login.enterPassword(password);
+		login.selectLanguage(language);
 		login.loginButton();
-		Assert.assertEquals(login.getPagename(), "Patient: None" );
+		Assert.assertEquals(login.getPagename(),  expectedValue);
 	}
 
 }
--- src/test/java/com/google/utilities/DataProviderUtils.java
+++ src/test/java/com/google/utilities/DataProviderUtils.java
@@ -4,22 +4,41 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviderUtils {
 	
-		@DataProvider
-		public Object[][] validData()
-		{
-			Object[][] main=new Object[3][2];
-			//i-->no of test case
-			//j-->no of parameter
-			main[0][0]="peter";
-			main[0][1]="peter123";
-			
-			main[1][0]="john";
-			main[1][1]="john123";
-			
-			main[2][0]="king";
-			main[2][1]="king123";
-			
-			return main;
-		}
+	@DataProvider
+	public Object[][] validCredentialData()
+	{
+		Object[][] main=new Object[2][4];
+		main[0][0]="admin";
+		main[0][1]="pass";
+		main[0][2]="English (Standard)";
+		main[0][3]="Patient: None";
+		
+		main[1][0]="physician";
+		main[1][1]="physician";
+		main[1][2]="English (Standard)";
+		main[1][3]="Patient: None";
+		
+		return main;
+	}
+
+	@DataProvider
+	public Object[][] validData() {
+		Object[][] main = new Object[4][2];
+		// i-->no of test case
+		// j-->no of parameter
+		main[0][0] = "peter";
+		main[0][1] = "peter123";
+
+		main[1][0] = "john";
+		main[1][1] = "john123";
+
+		main[2][0] = "king";
+		main[2][1] = "king123";
+
+		main[3][0] = "mark";
+		main[3][1] = "mark123";
+
+		return main;
+	}
 
 }
